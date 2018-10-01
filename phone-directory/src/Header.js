7a417016a36914453678f88a5819ef5921e557d4
swaroopchirravuri <swaroopchirravuri@gmail.com>
2018-10-01 23:19:48 +0530

    Created ‘Header’ Component

---
+++ phone-directory/src/Header.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+const Header = function() {
+    return (
+        <div className="header">
+          Phone Directory
+        </div>
+    )
+ }
+
+// class Header extends Component {
+//    render() {
+//        return (
+//            <div className="header">
+//                Phone Directory
+//            </div>
+//        )
+//    }
+// }
+
+ export default Header;
