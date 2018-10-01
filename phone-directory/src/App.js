dd2e981985ad2c452b198de05e5e9a667bc98cad
swaroopchirravuri <swaroopchirravuri@gmail.com>
2018-10-01 17:38:54 +0530

    Created Skeleton of Home Page

--- phone-directory/src/App.js
+++ phone-directory/src/App.js
@@ -3,8 +3,16 @@ import React, { Component } from 'react';
 class App extends Component {
   render() {
     return (
-      <div >
-       phone directory
+      <div>
+        Phone Directory
+        <div className="header">
+          Phone Directory
+        </div>
+        <button>Add</button>
+        <div>
+          <span>Name</span><br />
+          <span>Phone</span>
+        </div>
       </div>
     );
   }
