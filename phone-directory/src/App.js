8e39c8cc4ef939a8e0da8217cd8f6708cc4474b1
swaroopchirravuri <swaroopchirravuri@gmail.com>
2018-10-07 22:27:45 +0530

    Added ‘Delete’ button

--- phone-directory/src/App.js
+++ phone-directory/src/App.js
@@ -1,15 +1,46 @@
 import React, { Component } from 'react';
-import Header from './Header';
+import Header from './Header.js';
+import './App.css';
 
 class App extends Component {
+
   render() {
+
+    let subscribers = [
+      {
+        id: 1,
+        name: "Shilpa Bhat",
+        phone: "8888888888"
+      },
+      {
+        id: 2,
+        name: "Srishti Gupta",
+        phone: "9999999999"
+      }
+    ];
+
     return (
       <div>
-       <Header/>
-        <button>Add</button>
-        <div>
-          <span>Name</span><br />
-          <span>Phone</span>
+        <Header />
+        <div className="component-body-container">
+          <button className="custom-btn add-btn">Add</button>
+
+          <div className="grid-container heading-container">
+            <span className="grid-item name-heading">Name</span>
+            <span className="grid-item phone-heading">Phone</span>
+          </div>
+
+          {
+            subscribers.map(sub => {
+              return <div key={sub.id} className="grid-container">
+                <span className="grid-item">{sub.name}</span>
+                <span className="grid-item">{sub.phone}</span>
+                <span className="grid-item action-btn-container">
+                  <button className="custom-btn delete-btn">Delete</button>
+                </span>
+              </div>
+            })
+          }
         </div>
       </div>
     );
